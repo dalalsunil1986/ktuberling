13da1eb112114f41615a4ce087c291f10499d0bf
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2017-12-27 15:45:37 +0100

    Fix tests setup

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,4 +1,13 @@
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+jest.mock('../app', () => 'App');
+
+const ReactDom = require('react-dom');
+
+require('../index');
+
 describe('App', () => {
   it('init', () => {
+    expect(ReactDom.render).toHaveBeenCalledTimes(1);
+    expect(ReactDom.render.mock.calls).toMatchSnapshot();
   });
 });
